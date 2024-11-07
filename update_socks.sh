c3b45b8ba8f983618f14213bde9c14afdc7ab9fa
siruoren <zhuguojun6@163.com>
2024-11-07 13:42:36 +0800

    Create update_socks.sh

---
+++ update_socks.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+>socks5_list 
+for line in `awk -F '\{' '{for(i=1;i<=NF;i++) print$i}' socks.json `
+do
+  socks_line=""
+  for item in `echo ${line}|awk -F','  '{for(i=1;i<=NF;i++) print$i}'|sed 's/\[//g'|sed 's/\]//g'|grep -vE '}|{'`
+    do
+      if [ `echo "${item}"|grep 'ip'` ];then
+               item_ip=`echo ${item}|awk -F':' '{print$2}'|sed 's/"//g'`
+               socks_line=${socks_line}${item_ip}
+      fi
+      if [ `echo "${item}"|grep "port"` ];then
+               item_port=`echo ${item}|awk -F':' '{print$2}'|sed 's/"//g'`
+               socks_line="${socks_line}:${item_port}"
+      fi
+      if [ `echo "${item}"|grep "country"` ];then
+               item_cn=`echo ${item}|awk -F':' '{print$2}'|sed 's/"//g'`;
+               socks_line="${socks_line}#${item_ip}-${item_cn}"
+       fi;
+    done;
+  echo "socks5://${socks_line}" >> socks5_list;
+
+done
