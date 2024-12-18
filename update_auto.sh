0c8ca6ca977890b730a9a3eaf5beedfea1f72884
Your Name <you@example.com>
2024-12-18 10:13:33 +0800

    up

--- update_auto.sh
+++ update_auto.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
->other_list 
+>other_list.txt
 for line in `awk -F '{' '{for(i=1;i<=NF;i++) print$i}' socks.json `
 do
   socks_line=""
@@ -21,4 +21,18 @@ do
 if [ "${socks_line}" != "" ];then
   echo "socks5://${socks_line}" >> other_list;
 fi
+done
+
+present_list=`cat proxy.txt`
+
+for file in `ls proxy_list`
+do
+  cat ${file}|while read line
+  do
+    if [ `cat proxy.txt |grep \"${line}\" ` ];then
+      echo "$line is present,skip......"
+    else
+      echo "$line" >> proxy.txt
+    fi
+  done
 done
