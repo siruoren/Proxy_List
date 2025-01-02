aac63b36ad239bd502e5f1905ceb1ae702889f2e
Your Name <you@example.com>
2025-01-02 17:04:53 +0800

    update

--- update_auto.sh
+++ update_auto.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+## tg proxy
+check_url='https://telegram.org'
 >tg_list.txt
 for line in `awk -F '{' '{for(i=1;i<=NF;i++) print$i}' socks.json `
 do
@@ -7,22 +9,37 @@ do
     do
       if [ `echo "${item}"|grep 'ip'` ];then
                item_ip=`echo ${item}|awk -F':' '{print$2}'|sed 's/"//g'`
-               socks_line=${socks_line}${item_ip}
+               
       fi
       if [ `echo "${item}"|grep "port"` ];then
                item_port=`echo ${item}|awk -F':' '{print$2}'|sed 's/"//g'`
-               socks_line="${socks_line}:${item_port}"
+               echo "  - addr: ${item_ip}:${item_port}" >> tg_list.txttmp
+               echo "    check_config:" >> tg_list.txttmp
+               echo "      check_url: ${check_url}" >> tg_list.txttmp
+               echo "      initial_alive: true" >> tg_list.txttmp
+               echo "      timeout: 3" >> tg_list.txttmp
+               echo "" >>  tg_list.txttmp
       fi
-      if [ `echo "${item}"|grep "country"` ];then
-               item_cn=`echo ${item}|awk -F':' '{print$2}'|sed 's/"//g'`;
-               socks_line="${socks_line}#${item_ip}-${item_port}-${item_cn}"
-       fi;
+
     done;
-if [ "${socks_line}" != "" ];then
-  echo "socks5://${socks_line}" >> tg_list.txt;
-fi
+
 done
 
+#server config
+echo "server:" >>tg_list.txt
+
+echo "  socks5:">>tg_list.txt
+echo"     addr: \":18081\" " >>tg_list.txt
+
+echo "backends:">>tg_list.txt
+cat tg_list.txttmp >> tg_list.txt
+rm -f tg_list.txttmp
+
+
+
+
+
+### net proxy
 > proxy.txt
 
 for file in `find proxy_list/* -type f`
