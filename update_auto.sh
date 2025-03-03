df3dfa7c9dd2c1cebf3cf0be9d1c033d77fe0ba7
Your Name <you@example.com>
2025-03-03 11:15:06 +0800

    ADD banyunxiaoxi

--- update_auto.sh
+++ update_auto.sh
@@ -57,22 +57,3 @@ else
 fi;
 
 
-#from banyunxiaoxi.icu
-yesterday=`date -d"yesterday" +%F`
-echo $yesterday
-curl -v -L -k https://banyunxiaoxi.icu
-yesterday_url=`curl -L -k https://banyunxiaoxi.icu|grep ${yesterday}|grep 'href'|awk -F'href=' '{print$2}'|awk -F'>' '{print$1}'|grep -v 'category'|sed 's/"//g'|sort|uniq|tail -1`
-
-echo "yesterday_url:${yesterday_url}";
-if [ `curl -L -k ${yesterday_url}|grep '^vmess'|sed "s/<.*//g"|sort|uniq |wc -l` -eq '0' ];then
-    echo "remote banyunxiaoxi rss error,skip update!!!"
-
-else
-
-  if [ "${weekday}" == "6" ];then
-    > banyunxiaoxi.txt
-  fi;
-
-  curl ${yesterday_url}|grep '^vmess'|sed "s/<.*//g"|sort|uniq >> banyunxiaoxi.txt
-
-fi;
