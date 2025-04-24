7c84c32a0c8c88269e93fd990c28a25dac42771c
siruoren <zhuguojun6@163.com>
2025-04-24 19:46:10 +0800

    更新 banyunxiaoxi.sh

--- scripts/banyunxiaoxi.sh
+++ scripts/banyunxiaoxi.sh
@@ -23,6 +23,7 @@ echo "yesterday_url:${yesterday_url}";
 echo "";
 echo "today_url:${today_url}";
 echo "";
+> ../clashnodes.txt
 if [ \'${yesterday_url}\' != '' ] || [\' ${today_url}\' != '' ];then
     if [ "${weekday}" == "6" ];then
       > ../clashnodes.txt
@@ -42,7 +43,7 @@ while read line || [ -n $line ]
     do
         line_content=`echo $line |awk -F'/' '{printf$NF}'`
        if [ $line_content != '' ];then
-       echo $line_content
+       #echo $line_content
         if [ `cat ../clashnodes.txt|grep  $line_content|wc -l` = 0 ];then
             echo $line >> ../clashnodes.txt
         fi
