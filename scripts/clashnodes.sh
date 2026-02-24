6cd5b82cba7aa105e52c240d83aeb42d3a26ed62
siruoren <zhuguojun6@163.com>
2026-02-24 17:16:52 +0800

    up

--- scripts/clashnodes.sh
+++ scripts/clashnodes.sh
@@ -4,9 +4,12 @@
 cd $(dirname $0);
 nowdate=`date +%Y%m%d`
 weekday=`date +%w`
+curl_url_header="curl -s -k "
 
 
-if [ `curl -s -k https://clashgithub.com/wp-content/uploads/rss/${nowdate}.txt|wc -l` -eq '0' ];then
+echo "https://clashgithub.com/clashnode-${nowdate}.html"
+
+if [ `${curl_url_header} https://clashgithub.com/clashnode-${nowdate}.html|wc -l` -eq '0' ];then
    echo "remote clashnodes rss error,skip update!!!"
 
 else
@@ -16,7 +19,7 @@ else
   fi;
 
   cat ../clashnodes.txt > clashnodes.txttmp
-  curl -k -s https://clashgithub.com/wp-content/uploads/rss/${nowdate}.txt >> clashnodes.txttmp
+  ${curl_url_header}  https://clashgithub.com/clashnode-${nowdate}.html >> clashnodes.txttmp
 
 
   cat clashnodes.txttmp|sort -r |uniq > ../clashnodes.txt
