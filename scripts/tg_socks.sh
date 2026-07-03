b05fd8c3bd189ad685a8aeeffb1e169b84dc61de
siruoren <zhuguojun6@163.com>
2026-07-03 16:37:23 +0800

    fix(scripts/tg_socks.sh): 简化SOCKS代理列表处理逻辑
    移除了原有的JSON格式解析代码，直接使用原始代理文本格式进行处理，同时修正了文件命名错误

--- scripts/tg_socks.sh
+++ scripts/tg_socks.sh
@@ -7,38 +7,19 @@ cd $(dirname $0);
 #   fi;
 # fi
 cp ../tg_list.txt tg_list.txttmp;
-rm -f socks.json && wget https://raw.githubusercontent.com/hookzof/socks5_list/master/proxy.txt -O socks.json
+rm -f socks.txt && wget https://raw.githubusercontent.com/hookzof/socks5_list/master/proxy.txt -O socks.txt
 
-for line in `awk -F '{' '{for(i=1;i<=NF;i++) print$i}' socks.json `
+for line in `cat socks.txt`
 do
-  socks_line=""
-  for item in `echo ${line}|awk -F','  '{for(i=1;i<=NF;i++) print$i}'|sed 's/\[//g'|sed 's/\]//g'|grep -vE '}|{'`
-    do
-      if [ `echo "${item}"|grep 'ip'` ];then
-               item_ip=`echo ${item}|awk -F':' '{print$2}'|sed 's/"//g'`
-               socks_line=${socks_line}${item_ip}
-      fi
-      if [ `echo "${item}"|grep "port"` ];then
-               item_port=`echo ${item}|awk -F':' '{print$2}'|sed 's/"//g'`
-               socks_line="${socks_line}:${item_port}"
-      fi
-      if [ `echo "${item}"|grep "country"` ];then
-               item_cn=`echo ${item}|awk -F':' '{print$2}'|sed 's/"//g'`;
-               socks_line="${socks_line}#${item_ip}-${item_port}-${item_cn}"
-       fi;
-    done;
-if [ "${socks_line}" != "" ];then
-    if [[ `cat tg_list.txttmp|grep "${socks_line}"|wc -l` == "0" ]];then
-      echo "socks5://${socks_line}" >> tg_list.txttmp;
-    fi
-  
-fi
+  if [ "${line}" != "" ];then
+    echo "socks5://${line}" >> tg_list.txttmp;
+  fi
 done
 
 cat tg_list.txttmp|tail -200  > ../tg_list.txt
 
 rm -f tg_list.txttmp
-rm -f socks.json;
+rm -f socks.txt;
 
 # rm -f socks5.json && wget https://raw.githubusercontent.com/SoliSpirit/proxy-list/refs/heads/main/socks5.txt -O socks5.json
 
