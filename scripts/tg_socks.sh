4699fe57cb15a4094984df0f98d2d198c4d2805e
siruoren <zhuguojun6@163.com>
2026-07-03 18:06:17 +0800

    refactor(scripts/tg_socks.sh): 重构代理脚本，改用配置文件拉取代理源
    重构tg_socks.sh脚本，将硬编码的代理源地址改为从socks5/list_url.txt读取，同时清理冗余代码和注释

--- scripts/tg_socks.sh
+++ scripts/tg_socks.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
 cd $(dirname $0);
-# weekday=`date +%w`
-# if [[ `curl -k -s https://raw.githubusercontent.com/hookzof/socks5_list/master/tg/socks.json` != "" ]];then
-#   if [ "${weekday}" == "6" ];then
-#     > ../tg_list.txt
-#   fi;
-# fi
-cp ../tg_list.txt tg_list.txttmp;
-rm -f socks.txt && wget https://raw.githubusercontent.com/hookzof/socks5_list/master/proxy.txt -O socks.txt
+for url in `cat ../socks5/list_url.txt`
+do
+rm -f socks.txt && wget ${url} -O socks.txt
 
 for line in `cat socks.txt`
 do
@@ -17,13 +12,12 @@ do
   fi
 done
 
-cat tg_list.txttmp|tail -200  > ../tg_list.txt
+rm -f socks.txt;
+done
+
+cat tg_list.txttmp|sort|uniq > ../tg_list.txt
 
 rm -f tg_list.txttmp
-rm -f socks.txt;
-
-# rm -f socks5.json && wget https://raw.githubusercontent.com/SoliSpirit/proxy-list/refs/heads/main/socks5.txt -O socks5.json
-
 # for line in `cat socks5.json`;do
 #   echo "socks5://${line}#${line}" >> tg_list.txttmp;
 # done
